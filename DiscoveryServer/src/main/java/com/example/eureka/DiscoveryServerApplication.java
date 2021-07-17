3de7dd645fc8586ce79913e67d7521c221c16733
ankushrajpal <ankushrajpal8@gmail.com>
2021-07-18 00:56:10 +0530

    package name change

--- DiscoveryServer/src/main/java/com/example/eureka/DiscoveryServerApplication.java
+++ DiscoveryServer/src/main/java/com/example/eureka/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package com.cognizant.eureka;
+package com.example.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
